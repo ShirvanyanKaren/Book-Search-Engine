bdb90acea6cb0bd55afc12bd334520518f07d2cb
ShirvanyanKaren <kshirvanyan2000@gmail.com>
2023-09-04 17:31:52 -0700

    added typeDefs with query and mutation

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,50 @@
+const typeDefs = `
+type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+    }
+type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+
+}
+    
+
+
+input inputBook {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
+    
+type Auth {
+    token: ID!
+    user: User
+}      
+    
+type Query {
+    users: [User]
+    user(username: String!): User
+    me: User
+    books(username: String): [Book]
+    book(bookId: ID!): Book
+}
+
+type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(input: inputBook!): User
+    removeBook(bookId: ID!): User
+}
+        
+        `;
